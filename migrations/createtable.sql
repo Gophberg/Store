eb9fe80b9675bb70b63f7980b647111cdb1cb29d
JoeGopher <john19rus@gmail.com>
2022-08-05 16:42:51 +0700

    docker-compose up to create and run postgres database

--- migrations/createtable.sql
+++ migrations/createtable.sql
@@ -1,15 +1,11 @@
--- DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS store;
 
 CREATE TABLE IF NOT EXISTS store (
     id bigserial not null primary key,
-    userid bigserial,
-    useremail text,
-    amount numeric(10, 2),
-    currency text,
-    creationdate timestamp,
-    updatedate timestamp,
-    status text
+    title text,
+    photo text,
+    price numeric(10, 2)
 );
 
-INSERT INTO transactions (userid, useremail, amount, currency, creationdate, updatedate, status) VALUES (1, 'joe@mail.edu', 123.45, 'USD', TIMESTAMP '2022-06-19 21:00:00', TIMESTAMP '2022-06-19 21:00:01', 'New');
-INSERT INTO transactions (userid, useremail, amount, currency, creationdate, updatedate, status) VALUES (3, 'jane@mail.edu', 345.67, 'RUR', TIMESTAMP '2022-06-19 21:00:03', TIMESTAMP '2022-06-19 21:00:04', 'Canceled');
+INSERT INTO store (title, photo, price) VALUES ('Some Title', './path/to/photo.jpg', 123.45);
+INSERT INTO store (title, photo, price) VALUES ('Some Title', './path/to/photo.jpg', 345.67);

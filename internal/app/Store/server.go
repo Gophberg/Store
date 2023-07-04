d89aec488e43f8b6e6b7dacbe1d2c6afb00e8d4f
Gophberg <john19rus@gmail.com>
2023-07-04 16:13:42 +0700

    some clean

--- internal/app/Store/server.go
+++ internal/app/Store/server.go
@@ -7,11 +7,17 @@ import (
 var config Config
 
 func Start() error {
+
 	config.NewConfig()
+
 	s := Ad{}
+
 	//var qc *QueryCredentials
+
 	http.HandleFunc("/getAd", s.getAd)
 	http.HandleFunc("/getAllAds", s.getAllAds)
 	http.HandleFunc("/createAd", s.createAd)
+
 	return http.ListenAndServe(":9000", nil)
+
 }
